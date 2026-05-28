d0f903c9c5218553cb969d53c3f2c1f294bae858
Linkzenic <linkzenic@users.noreply.github.com>
2026-05-28 13:45:02 -0500

    Allow unsupported ROM extraction with warning

--- mm/2s2h/Extractor/Extract.h
+++ mm/2s2h/Extractor/Extract.h
@@ -48,8 +48,10 @@ class Extractor {
     void GetRoms(std::vector<std::string>& roms);
     void ShowSizeErrorBox() const;
     void ShowCrcErrorBox() const;
+    void ShowUnsupportedVersionErrorBox() const;
     void ShowCompressedErrorBox() const;
     int ShowRomPickBox(uint32_t verCrc) const;
+    bool ConfirmUnsupportedRom() const;
     bool ManuallySearchForRom();
     bool ManuallySearchForRomMatchingType(RomSearchMode searchMode);
 
