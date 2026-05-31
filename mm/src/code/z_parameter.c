f9ac2107d0e8bb4553e8e60ab2bcb25bd381905f
Linkzenic <linkzenic@users.noreply.github.com>
2026-05-30 17:03:28 -0700

    Fix Infinite Epona Carrots hook

--- mm/src/code/z_parameter.c
+++ mm/src/code/z_parameter.c
@@ -8053,7 +8053,8 @@ void Interface_DrawMinigameIcons(PlayState* play) {
 
     if ((play->pauseCtx.state == PAUSE_STATE_OFF) && (play->pauseCtx.debugEditor == DEBUG_EDITOR_NONE)) {
         // Carrots rendering if the action corresponds to riding a horse
-        if (interfaceCtx->aButtonHorseDoAction == DO_ACTION_FASTER) {
+        if (interfaceCtx->aButtonHorseDoAction == DO_ACTION_FASTER &&
+            GameInteractor_Should(VB_CONSUME_EPONA_CARROT, true)) {
             // Load Carrot Icon
             gDPLoadTextureBlock(OVERLAY_DISP++, gCarrotIconTex, G_IM_FMT_RGBA, G_IM_SIZ_32b, 16, 16, 0,
                                 G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK,
